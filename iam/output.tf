8e05b67f339621c4ecd7eab59ad1d0042cac48be
Murtuza Choonawala <murtu.c.83@gmail.com>
2021-06-29 18:28:33 +0530

    Add files via upload

---
+++ iam/output.tf
@@ -0,0 +1,4 @@
+output "role" {
+    value = aws_iam_role.role.arn
+
+}
--- iam/role.tf
+++ iam/role.tf
@@ -1,1 +1,36 @@
 
+resource "aws_iam_role" "role" {
+name = var.role
+assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+
+
+resource "aws_iam_policy_attachment" "lambda-attach" {
+  name       = "lambda"
+  
+  roles      = [aws_iam_role.role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy_attachment" "s3-attach" {
+  name       = "s3"
+  
+  roles      = [aws_iam_role.role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
